dc1c4a90a99b9421c929013bd67a6131f563d77a
VergariMussolini <63793732+VergariMussolini@users.noreply.github.com>
2020-05-12 08:49:04 +0100

    Add files via upload

--- public/GameCode/Stages/Npc.js
+++ public/GameCode/Stages/Npc.js
@@ -1,76 +1,107 @@
 let img;
 let npc1;
-let move=true;
+let move = true;
 let mx;
 let my;
+let NpcTileX;
+let NpcTileY;
+
 
 let npcPath;
+let RandomFood = 6;
 
-const SetUpNpc = () => {
+const Npcsetup = () => {
   createCanvas(800, 600);
   npcPath = [createVector(0, 200), createVector(200, 200), createVector(100, 100), createVector(200, 300), createVector(350, 350)];
-  
-  npc1 = new npc(npcPath[0].x+350, npcPath[0].x+600);
-
+  npc1 = new npc(npcPath[0].x+600, npcPath[0].x+900);
 }
 
 const DrawNpc = () => {
-  background(220);
-  ///////////////////////////
-    fill(100);
+
+    fill(255);
     npc1.draw_npc();
-//dentro do restaurante E fora de uma mesa
-  if(npc1.get_posX() > 200 && npc1.get_posX() < 700 && npc1.get_posY() >= 100 && npc1.get_posY() <= 500){
-      if(move){
-        if (npc1.get_posX() < mx) {
-            npc1.moveX(1);
-        }
-        if (npc1.get_posY() < my) {
-            npc1.moveY(1);
-        }
-        if (npc1.get_posX() > mx) {
-            npc1.moveX(-1);
-        }
-        if (npc1.get_posY() > my) {
-            npc1.moveY(-1);
-        }
-        if (npc1.get_posX() > ch.x &&  npc1.get_posX() < ch.x + ch.w &&  
-            npc1.get_posY() > ch.y  &&  npc1.get_posY() < ch.y + ch.h){
+  
+    //SE NPC = DO TILE DA CADEIRA ENTÃO ELE SE MOVE
+
+    mouseOnTileFun();
+    for(let i = 0;  i < RestaurantDefaultData.TilesX ;i++){
+      for(let j = 0; j < RestaurantDefaultData.TilesY; j++){
+
+        let LocalPositions = CalculateRestaurantTile(i, j);
+
+        if(i == npc1.get_posX() && j == npc1.get_posY()){
           TimeToEat();
+          console.log(1);
+        }else if(move){
+            if (npc1.get_posX() < mx) {
+                npc1.moveX(1);
+            }
+            if (npc1.get_posY() < my) {
+                npc1.moveY(1);
+            }
+            if (npc1.get_posX() > mx) {
+                npc1.moveX(-1);
+            }
+            if (npc1.get_posY() > my) {
+                npc1.moveY(-1);
+            
+          }
+        };
+      };
+    };
+  
+/*
+    if(npc1.get_posX() < Itemx && npc1.get_posX() > itemx+1 && npc1.get_posY() < Itemy && npc1.get_posY() > Itemy+1 ){
+        if(move){
+          if (npc1.get_posX() < mx) {
+              npc1.moveX(1);
+          }
+          if (npc1.get_posY() < my) {
+              npc1.moveY(1);
+          }
+          if (npc1.get_posX() > mx) {
+              npc1.moveX(-1);
+          }
+          if (npc1.get_posY() > my) {
+              npc1.moveY(-1);
+          }
         }
       }
-  }
-  if(npc1.get_posY() >= 500 ) {
-    
-    go_up();
-    
-  }
-  if(npc1.get_posY() <= 100 ) {
-    
-    go_down();
-    
-  }
-  
-  if(npc1.get_posX() <= 200 ) {
-    
-    go_rigth();
-    
-  }
-  
-  if(npc1.get_posX() >= 700 ) {
-    
-    go_left();
-    
-  }
-  
-  console.log(DishRequest[4]);
-}
-const mousePressed = () => {
+      if (npc1.get_posX() >= furniture[1].x &&  npc1.get_posX() < furniture[1].x + 64 && npc1.get_posY() > furniture[1].y  &&  npc1.get_posY() < furniture[1].y + 64){
+          TimeToEat();
+          console.log(1);
+        }
+        if (npc1.get_posX() >= furniture[2].x &&  npc1.get_posX() < furniture[2].x + 64 && npc1.get_posY() > furniture[2].y  &&  npc1.get_posY() < furniture[2].y + 64){
+          TimeToEat();
+          console.log(2);
+        }
+        if (npc1.get_posX() >= furniture[3].x &&  npc1.get_posX() < furniture[3].x + 64 && npc1.get_posY() > furniture[3].y  &&  npc1.get_posY() < furniture[3].y + 64){
+          TimeToEat();
+          console.log(1);
+        }
+*/
 
+  if(npc1.get_posY() >= 181 ) {
+    go_up();
+  }
+  if(npc1.get_posY() <= 692 ) {
+    go_down();
+  }
+  
+  if(npc1.get_posX() <=  width/2 - 416 ) {
+    go_rigth();
+  }
+  
+  if(npc1.get_posX() >=  width/2 + 426 ) {
+    go_left();
+  }
+  
+}
+
+const NpcPressed = () => {
     mov = true;
     mx = mouseX;
     my = mouseY;
-
 }
 
 const TimeToEat = () => {
@@ -89,13 +120,9 @@ const TimeToEat = () => {
 const leave = () => {
     npc1.posX = -50;
     npc1.posY = -50; 
-    // Enviar o dinheiro para o database e - comida X
-    httpPost('/post/NpcPay', {id : UserID, money: amount}, data =>{
-      NpcGiveMoney();
-    });
-    httpPost('/post/NpcConsume', {id : UserID, dishes: amount}, data =>{
-      NpcConsumeDish();
-    });
+    
+    // Enviar o dinheiro, xp, e -1 comida para o database e - comida X
+    NpcConsume();
 }
 
 const go_up = () => {
