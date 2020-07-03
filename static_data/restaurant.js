6869e62845303d31ee6745e581855e8c3330dfc4
VergariMussolini <63793732+VergariMussolini@users.noreply.github.com>
2020-07-03 12:56:58 +0100

    Final Project

--- static_data/restaurant.js
+++
@@ -1,9 +0,0 @@
-const RestaurantDetails = {
-
-    sizeX: 13, //Restaurant size on X in tiles
-    sizeY: 7, //Restaurant size on Y in tiles
-    tilesSize: 64 //Tiles size in pixels
-
-};
-
-module.exports = RestaurantDetails;
